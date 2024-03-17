12910d253b2ee843c82146f5d7d96a5b54c8fed7
Joseph Enoch <enochjoseph01@gmail.com>
2024-03-17 15:25:15 +0100

    install: react testing library

--- components/secondary/index.tsx
+++ components/secondary/index.tsx
@@ -1,4 +1,4 @@
 export {default as Header} from "./Header"
 export {default as Task} from "./Task"
-export {default as CreateTask} from "./CreateTask"
+export {default as CreateTask} from "./CreateTask/CreateTask"
 export {default as CreateTaskModal} from "./CreateTaskModal"
